d3961b77bf39b398ac1d5faf2da2c315d6148d8c
William Bernardoni <wbernar5@jhu.edu>
2018-04-26 14:11:11 -0400

    Added script to boot gazebo and load model, and added xacro compilation into CMakeList

---
+++ scripts/load.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $(rospack find robot_tutorial_3)/urdf
+
+x-terminal-emulator -e roslaunch gazebo_ros empty_world.launch 
+
+rosrun gazebo_ros spawn_model -file rover.urdf -urdf -model rover
+
+rosrun robot_tutorial_1 keyin rover
+
